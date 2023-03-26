607049653c54db95e04e3fa08a6e1fbab61bcee4
DESKTOP-S4BD7LK\Ultrabook Lenovo <daniel_thls@yahoo.com.br>
2023-03-26 13:23:27 -0300

    inclui aula de fixação de saída de dados

--- Exercicio1/src/Main.java
+++ Exercicio1/src/Main.java
@@ -1,9 +1,26 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Teste teste");
+		int y = 32;
+		double x = 10.35784;
+		String nome = "Maria";
+		int idade = 31;
+		double renda = 4387.898687;
 
+		System.out.println("Zug zug");
+		System.out.println("Teste");
+		System.out.println(y);
+		System.out.println(x);
+		System.out.printf("%.2f%n", x); // %.<numero de casas decimais>f e depois %n para quebrar linha
+		System.out.printf("%.4f%n", x);
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f%n", x);
+		System.out.println("Resultado: " + x + " metros");
+		System.out.printf("Resultado: %.2f metros%n", x);
+
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais por mês%n", nome, idade, renda); // %s String %d Integer
 	}
 
 }
